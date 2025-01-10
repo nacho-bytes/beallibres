3cee610cb07c5f2fca6ec68b2b494f9f9204bc34
nacho <nachodiezlambies@gmail.com>
2025-01-10 10:55:11 +0100

    Added router_bloc to control the avaliabled pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:firebase_core/firebase_core.dart' show Firebase;
-import 'package:flutter/material.dart' show BuildContext, MaterialApp, StatelessWidget, Widget, WidgetsFlutterBinding, runApp;
-import 'package:flutter_gen/gen_l10n/app_localizations.dart' show AppLocalizations;
+import 'package:flutter/material.dart'
+    show
+        BuildContext,
+        Icons,
+        MaterialApp,
+        StatelessWidget,
+        Widget,
+        WidgetsFlutterBinding,
+        runApp;
+import 'package:flutter_bloc/flutter_bloc.dart' show BlocBuilder, BlocProvider;
+import 'package:flutter_gen/gen_l10n/app_localizations.dart'
+    show AppLocalizations;
+import 'package:go_router/go_router.dart';
 
+import 'adaptative_navigation_trail.dart'
+    show
+        AdaptativeNavigationDestination,
+        AdaptiveNavigationTrail;
 import 'firebase_options.dart' show DefaultFirebaseOptions;
-import 'router/router.dart' show router;
+import 'router/router_bloc.dart' show RouterBloc, RouterState;
+import 'router/routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,22 +30,66 @@ void main() async {
 }
 
 /// The main application widget.
-/// 
+///
 /// This widget is the root of the application.
 class MyApp extends StatelessWidget {
   /// Creates the main application widget.
-  /// 
+  ///
   /// The [key] is optional and defaults to `null`.
   const MyApp({
     super.key,
   });
 
   @override
-  Widget build(final BuildContext context) => MaterialApp.router(
-      routerConfig: router,
-      onGenerateTitle: (final BuildContext context) =>
-          AppLocalizations.of(context)!.appTitle,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-    );
+  Widget build(final BuildContext context) => BlocProvider<RouterBloc>(
+    create: (final BuildContext context) => RouterBloc(
+      initialState: RouterState(
+        destinations: <AdaptativeNavigationDestination>[
+          AdaptativeNavigationDestination(
+            title: 'Home',
+            icon: Icons.home,
+            // location: '/',
+            location: const HomeRoute().location,
+          ),
+          AdaptativeNavigationDestination(
+            title: 'Profile',
+            icon: Icons.person,
+            location: const ProfileRoute().location,
+          ),
+          AdaptativeNavigationDestination(
+            title: 'Admin',
+            icon: Icons.admin_panel_settings,
+            location: const AdminRoute().location,
+          ),
+        ],
+      ),
+    ),
+    child: BlocBuilder<RouterBloc, RouterState>(
+      builder: (
+        final BuildContext context,
+        final RouterState routerState,
+      ) => MaterialApp.router(
+        routerConfig: GoRouter(
+          initialLocation: const HomeRoute().location,
+          routes: <RouteBase>[
+            ShellRoute(
+              builder: (
+                final BuildContext context,
+                final GoRouterState state,
+                final Widget child,
+              ) => AdaptiveNavigationTrail(
+                destinations: routerState.destinations,
+                child: child,
+              ), 
+              routes: $appRoutes,
+            ),
+          ],
+        ),
+        onGenerateTitle: (final BuildContext context) =>
+            AppLocalizations.of(context)!.appTitle,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+      ),
+    ),
+  );
 }
---
+++ lib/router/router_bloc.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart' show Bloc, Emitter;
+
+import '../adaptative_navigation_trail.dart' show AdaptativeNavigationDestination;
+import 'routes.dart';
+
+class RouterState {
+  const RouterState({
+    this.destinations = const <AdaptativeNavigationDestination>[],
+  });
+
+  final List<AdaptativeNavigationDestination> destinations;
+
+  RouterState copyWith({
+    final List<AdaptativeNavigationDestination>? destinations,
+  }) => RouterState(
+    destinations: destinations ?? this.destinations,
+  );
+}
+
+sealed class RouterEvent {}
+
+final class AddAdminDestinationEvent extends RouterEvent {}
+
+final class RemoveAdminDestinationEvent extends RouterEvent {}
+
+class RouterBloc extends Bloc<RouterEvent, RouterState> {
+  RouterBloc({
+    final RouterState? initialState,
+  }) : super(initialState ?? const RouterState()) {
+    on<AddAdminDestinationEvent>(_onAddAdminDestinationEvent);
+    on<RemoveAdminDestinationEvent>(_onRemoveAdminDestinationEvent);
+  }
+
+  void _onAddAdminDestinationEvent(
+    final AddAdminDestinationEvent event,
+    final Emitter<RouterState> emit,
+  ) {
+    if (state.destinations.any(
+      (final AdaptativeNavigationDestination destination) =>
+        destination.location == const AdminRoute().location,
+    )) {
+      return;
+    }
+
+    emit(
+      state.copyWith(
+        destinations: List<AdaptativeNavigationDestination>.from(
+          state.destinations
+            ..add(
+              AdaptativeNavigationDestination(
+                title: 'Admin',
+                icon: Icons.admin_panel_settings,
+                location: const AdminRoute().location,
+              ),
+            ),
+        ),
+      ),
+    );
+  }
+
+  void _onRemoveAdminDestinationEvent(
+    final RemoveAdminDestinationEvent event,
+    final Emitter<RouterState> emit,
+  ) {
+    emit(
+      state.copyWith(
+        destinations: List<AdaptativeNavigationDestination>.from(
+          state.destinations
+            ..removeWhere(
+              (final AdaptativeNavigationDestination destination) =>
+                destination.location == const AdminRoute().location,
+            ),
+        ),
+      ),
+    );
+  }
+}
